97df75128855b10226dc5479ad53243e47e8388f
ThielHater <pierre.beckmann@yahoo.de>
2016-07-12 08:48:20 +0200

    Checksumme für 1.08k mit FPS Patch hinzugefügt

--- GRMFixes/VersionCheck.h
+++ GRMFixes/VersionCheck.h
@@ -5,6 +5,7 @@ namespace VersionCheck
 	enum EGothicVersion
 	{
 		GV_Gothic_1_08k_mod = 0x008246d2,
+		GV_Gothic_1_08k_fps = 0x00828cab,
 		GV_Gothic_1_11f = 0x0085d9c3,
 		GV_Spacer_1_41 = 0x0091de75,
 		GV_Spacer_1_5 = -1,
